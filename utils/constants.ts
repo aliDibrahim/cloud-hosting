b2e5cb40ca777f3826b3e8dd20716e9b96dd47d4
aliDibrahim <aliebraheem2192001@gmail.com>
2026-01-30 02:28:37 +0300

    add production domain

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,9 @@
 export const pageSize = 6;
 
-// const PRODUCTION_DOMAIN = "https://cloud-hosting-project-course.vercel.app";
-const PRODUCTION_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = "https://cloud-hosting-sooty.vercel.app";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
-
 
 export const DOMAIN =
   process.env.NODE_ENV === "production"
     ? PRODUCTION_DOMAIN
     : DEVELOPMENT_DOMAIN;
- 
